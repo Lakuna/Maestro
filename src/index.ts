f906f4f82e98f774f4f303cd12adda4d94eb9144
Travis Martin <tjmartin2003@gmail.com>
2026-05-26 10:35:51 -0500

    More temporary logging.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 /* eslint-disable @typescript-eslint/prefer-readonly-parameter-types */
 import type { infer as zinfer } from "zod";
 
@@ -16,35 +17,42 @@ const app: Hono = new Hono();
 app.post("/api/interactions", zValidator("json", interaction), async (c) => {
 	const publicKey = process.env["DISCORD_PUBLIC_KEY"];
 	if (!publicKey) {
+		console.error("No public key.");
 		return c.json(void 0, 500);
 	}
 
 	const signature = c.req.header("X-Signature-Ed25519");
 	const timestamp = c.req.header("X-Signature-Timestamp");
 	if (!signature || !timestamp) {
+		console.error("No signature or timestamp.");
 		return c.json(void 0, 401);
 	}
 
 	// https://docs.discord.com/developers/interactions/overview#acknowledging-ping-requests
 	const body = await c.req.text();
+	console.log(body);
 	const verified = nacl.sign.detached.verify(
 		Buffer.from(timestamp + body),
 		Buffer.from(signature, "hex"),
 		Buffer.from(publicKey, "hex")
 	);
 	if (!verified) {
+		console.error("Not verified.");
 		return c.json(void 0, 401);
 	}
 
 	const data = c.req.valid("json");
+	console.log(data);
 
 	// https://docs.discord.com/developers/interactions/overview#acknowledging-ping-requests
 	if (data.type === InteractionType.PING) {
+		console.log("Pong.");
 		return c.json({ type: InteractionCallbackType.PONG } satisfies zinfer<
 			typeof interactionResponse
 		>);
 	}
 
+	console.log("Other.");
 	return c.json(void 0, 501);
 });
 
