7f2761b2550fae9fc5acc5131aede63008ffbc75
Travis Martin <tjmartin2003@gmail.com>
2026-05-26 10:52:15 -0500

    Revert temporary changes.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-/* eslint-disable no-console */
 /* eslint-disable @typescript-eslint/prefer-readonly-parameter-types */
 import type { infer as zinfer } from "zod";
 
+import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
 import nacl from "tweetnacl";
 
@@ -13,52 +13,38 @@ import InteractionType from "./discord/interactions/receivingAndResponding/Inter
 
 const app: Hono = new Hono();
 
-app.post("/api/interactions", async (c) => {
+app.post("/api/interactions", zValidator("json", interaction), async (c) => {
 	const publicKey = process.env["DISCORD_PUBLIC_KEY"];
 	if (!publicKey) {
-		console.error("No public key.");
 		return c.json(void 0, 500);
 	}
 
 	const signature = c.req.header("X-Signature-Ed25519");
 	const timestamp = c.req.header("X-Signature-Timestamp");
 	if (!signature || !timestamp) {
-		console.error("No signature or timestamp.");
 		return c.json(void 0, 401);
 	}
 
 	// https://docs.discord.com/developers/interactions/overview#acknowledging-ping-requests
 	const body = await c.req.text();
-	console.log(body);
 	const verified = nacl.sign.detached.verify(
 		Buffer.from(timestamp + body),
 		Buffer.from(signature, "hex"),
 		Buffer.from(publicKey, "hex")
 	);
 	if (!verified) {
-		console.error("Not verified.");
 		return c.json(void 0, 401);
 	}
 
-	const json: unknown = await c.req.json();
-	console.log(json);
-	const parse = interaction.safeParse(json);
-	if (!parse.success) {
-		console.error(parse.error);
-		return c.json(void 0, 400);
-	}
-	const { data } = parse;
-	console.log(data);
+	const data = c.req.valid("json");
 
 	// https://docs.discord.com/developers/interactions/overview#acknowledging-ping-requests
 	if (data.type === InteractionType.PING) {
-		console.log("Pong.");
 		return c.json({ type: InteractionCallbackType.PONG } satisfies zinfer<
 			typeof interactionResponse
 		>);
 	}
 
-	console.log("Other.");
 	return c.json(void 0, 501);
 });
 
